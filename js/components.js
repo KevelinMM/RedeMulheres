28334b769860a7357425ccaab1c8f97b0d39776b
Kevelin <89493394+KevelinMM@users.noreply.github.com>
2022-04-13 17:20:41 -0300

    Add files via upload

--- js/components.js
+++ js/components.js
@@ -6,19 +6,18 @@ class Header extends HTMLElement {
               <!-- Navigation-->
               <nav class="navbar bg-rede2 fixed-top" id="mainNav">
                   <div class="container">
-                      <a class="navbar-brand" href="#page-top">REDE FEMININA DE COMBATE AO CÂNCER</a>
+                      <a class="navbar-brand fs-5" href="../">REDE FEMININA DE COMBATE AO CÂNCER</a>
                       <button class="navbar-toggler text-uppercase font-weight-bold bg-rede text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                           Menu
                           <i class="fas fa-bars"></i>
                       </button>
                       <div class="collapse navbar-collapse" id="navbarResponsive">
                           <ul class="navbar-nav ms-auto">
-                              <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="apresentacao.html">Apresentação</a></li>
-                              <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="comoajudamos.html">Como Ajudamos</a></li>
-                              <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="depoimentos.html">Depoimentos</a></li>
-                              <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="doacao.html">Contribuições</a></li>
-                              <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="voluntariado.html">Voluntariado</a></li>
-                              <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="cadastro.html">Login</a></li>
+                              <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="/paginas/apresentacao.html">Apresentação</a></li>
+                              <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="/paginas/comoajudamos.html">Como Ajudamos</a></li>
+                              <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="/paginas/depoimentos.html">Depoimentos</a></li>
+                              <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="/paginas/doacao.html">Contribuições</a></li>
+                              <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="/paginas/voluntariado.html">Voluntariado</a></li>
                           </ul>
                       </div>
                   </div>
@@ -30,11 +29,11 @@ class Header extends HTMLElement {
                       <!-- Masthead Avatar Image-->
                       <img class="masthead-avatar mb-5" src="../assets/img/portfolio/rede.png" alt="Logo Rede Feminina de Combate ao Câncer de Fraiburgo " />
                       <!-- Masthead Heading-->
-                      <h1 class="masthead-heading text-uppercase mb-0">Rede Feminina de Combate ao Câncer</h1>
+                      <h1 class="masthead-heading fs-1 text-uppercase  mb-0">Rede Feminina de Combate ao Câncer</h1>
                       <!-- Icon Divider-->
                       <div class="divider-custom divider-light">
                           <div class="divider-custom-line"></div>
-                          <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                          <div class="divider-custom-icon"><i class="fa-solid fa-heart"></i></div>
                           <div class="divider-custom-line"></div>
                       </div>
                       <!-- Masthead Subheading-->
@@ -67,9 +66,9 @@ class Header extends HTMLElement {
                       <!-- Footer Social Icons-->
                       <div class="col-lg-4 mb-5 mb-lg-0">
                           <h4 class="text-uppercase mb-4">Redes Sociais</h4>
-                          <a class="btn btn-outline-light btn-social mx-1" href="https://www.facebook.com/rfccfraiburgo"><i class="fab fa-fw fa-facebook-f"></i></a>
-                          <a class="btn btn-outline-light btn-social mx-1" href="https://www.instagram.com/redefemininafraiburgo/"><i class="fa-brands fa-instagram"></i></i></a>
-                          <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fa-brands fa-whatsapp"></i></i></a>
+                          <a class="btn btn-outline-light btn-social mx-1" target="_blank" href="https://www.facebook.com/rfccfraiburgo"><i class="fab fa-fw fa-facebook-f"></i></a>
+                          <a class="btn btn-outline-light btn-social mx-1" target="_blank" href="https://www.instagram.com/redefemininafraiburgo/"><i class="fa-brands fa-instagram"></i></i></a>
+                          <a class="btn btn-outline-light btn-social mx-1" target="_blank" href="#!"><i class="fa-brands fa-whatsapp"></i></i></a>
                       </div>
                       <!-- Footer About Text-->
                       <div class="col-lg-4">
@@ -89,6 +88,8 @@ class Header extends HTMLElement {
           `;
         }
     }
+
+
 
 customElements.define('main-header', Header);
 customElements.define('main-footer', Footer);
