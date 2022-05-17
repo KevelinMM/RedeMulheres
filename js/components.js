e428554e10600c7fe3619f59bf8b05900a0eaa67
Uniplac <github@uniplaclages.edu.br>
2022-05-17 14:07:05 -0300

    logo e fotos

--- js/components.js
+++ js/components.js
@@ -6,7 +6,7 @@ class Header extends HTMLElement {
               <!-- Navigation-->
               <nav class="navbar bg-rede2 fixed-top" id="mainNav">
                   <div class="container">
-                      <a class="navbar-brand fs-5" href="../"><img src="../assets/LogoNova.png" style="width:55px; height:30px"/></a>
+                      <a class="navbar-brand fs-5" href="../"><img src="../assets/LogoB.png" style="width:55px; height:30px"/></a>
                       <button class="navbar-toggler text-uppercase font-weight-bold bg-rede text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                           Menu
                           <i class="fas fa-bars"></i>
